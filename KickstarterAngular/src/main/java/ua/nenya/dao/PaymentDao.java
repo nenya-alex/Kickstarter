963cb943bc5dfff112c8b485b3542d60155659a6
nenya-alex <alexni@ukr.net>
2016-08-11 17:24:27 +0300

    fixed CRLF

--- KickstarterAngular/src/main/java/ua/nenya/dao/PaymentDao.java
+++
@@ -1,9 +0,0 @@
-package ua.nenya.dao;
-
-import ua.nenya.domain.Payment;
-
-public interface PaymentDao {
-	
-	Payment writePaymentInProject(Payment payment);
-
-}
--- KickstarterAngular/src/main/java/ua/nenya/dao/QuoteDao.java
+++
@@ -1,8 +0,0 @@
-package ua.nenya.dao;
-
-import ua.nenya.domain.Quote;
-
-public interface QuoteDao {
-	
-	Quote getRandomQuote();
-}
